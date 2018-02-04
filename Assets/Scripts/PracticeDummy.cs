c5ac716cdd073143706dd2f4139d5118f92c8a6c
Vanessa Chu <vanessa.chu@outlook.com>
2018-02-03 22:37:04 -0500

    Created dodgeball

---
+++ Assets/Scripts/PracticeDummy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeDummy : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Projectile")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
