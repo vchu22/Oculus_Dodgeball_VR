d455851658d992fa3e9e21c8159996f25baf958e
Vanessa Chu <vanessa.chu@outlook.com>
2018-02-28 22:51:01 -0500

    made target prefeb destructible

---
+++ Assets/Scripts/GameStat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStat : MonoBehaviour {
+    public int score;
+    int totalTargets;
+    System.Random rand = new System.Random();
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        totalTargets = rand.Next(30);
+        Debug.Log("The room has a total of "+ totalTargets + " targets");
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void generateTargets()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace System
+{
+    public class Target : MonoBehaviour
+    {
+        private Vector3 destination = new Vector3();
+        private Random rand = new Random();
+        GameStat gameStat = GameObject.Find("GameUniverse").GetComponent<GameStat>();
+        public float speed;
+        // Use this for initialization
+        void Start()
+        {
+            setDestination();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Move();
+        }
+
+        // what do when collision occurs
+        private void OnCollisionEnter(Collision collisionInfo)
+        {
+            // target will disappear when hit by a ball
+            if (collisionInfo.collider.tag == "Ball")
+            {
+                Destroy(gameObject);
+                int gamescore = gameStat.score++;
+                Debug.Log("Score: "+ gamescore);
+            }
+            else
+            {
+                // change direct when hitting a concrete object
+                setDestination();
+            }
+        }
+        private void setDestination()
+        {
+            float x = rand.Next(-490, 490) * 0.01f,
+                y = 1.3f,
+                z = rand.Next(-490, 490) * 0.01f;
+            Debug.Log("x: " + x + ", y: " + y + ", z: " + z);
+            destination = new Vector3(x, y, z);
+        }
+        private void Move()
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, destination, step);
+        }
+    }
+}
