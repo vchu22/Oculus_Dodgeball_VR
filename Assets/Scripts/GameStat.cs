605cad88fe04ea711fa1f1cd20a0f13f1e1d91a9
Vanessa Chu <vchu22@users.noreply.github.com>
2019-05-31 14:53:37 -0400

    hide score from inspector

--- Assets/Scripts/GameStat.cs
+++ Assets/Scripts/GameStat.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameStat : MonoBehaviour {
-    public int score;
+    [HideInInspector] public int score;
     public GameObject targetPrefab;
     int totalTargets;
     System.Random rand = new System.Random();
