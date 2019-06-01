631f5a558e84d0a609669d54976595802dd990be
Vanessa Chu <vchu22@users.noreply.github.com>
2019-05-31 23:02:43 -0400

    autodetect if HMD is connected, add fallback mode

---
+++ Assets/Scripts/DetectVRHeadset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectVRHeadset : MonoBehaviour
+{
+    [SerializeField] private GameObject VRPlayerController;
+    [SerializeField] private GameObject NonVRPlayerController;
+    // Awake() runs before Start()
+    void Awake()
+    {
+        if (UnityEngine.XR.XRDevice.isPresent) {
+            Debug.LogWarning("VR Headset Found");
+            Instantiate(VRPlayerController, new Vector3(0,1,0), Quaternion.identity);
+        } else {
+            Debug.LogWarning("VR Headset Not Found");
+            Instantiate(NonVRPlayerController, new Vector3(0, 1, 0), Quaternion.identity);
+        }
+    }
+}
