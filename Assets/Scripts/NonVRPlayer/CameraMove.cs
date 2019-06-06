420aa38f0e94d5b2c47194b617e7cf78dec3877f
Vanessa Chu <vchu22@users.noreply.github.com>
2019-06-06 00:20:25 -0400

    replaced pre-built first person controller

---
+++ Assets/Scripts/NonVRPlayer/CameraMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 2.0f;
+    [SerializeField] private float smoothing = 2.0f;
+    
+    private GameObject player;
+    private Vector2 smoothedVelocity;
+    private Vector2 currentLookingPos;
+    private void Start(){
+        Cursor.lockState = CursorLockMode.Locked;
+        player = transform.parent.gameObject;
+    }
+    private void Update(){
+        RotateCamera();
+    }
+    private void RotateCamera(){
+        Vector2 inputValues = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        inputValues = Vector2.Scale(inputValues, new Vector2(mouseSensitivity * smoothing, mouseSensitivity * smoothing));
+        smoothedVelocity.x = Mathf.Lerp(smoothedVelocity.x, inputValues.x, 1f / smoothing);
+        smoothedVelocity.y = Mathf.Lerp(smoothedVelocity.y, inputValues.y, 1f / smoothing);
+
+        currentLookingPos += smoothedVelocity;
+
+        transform.localRotation = Quaternion.AngleAxis(-currentLookingPos.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(currentLookingPos.x, player.transform.up);
+    }
+}
