21d18d786786aefe1cca9607c3153d54e01b3c67
Vanessa Chu <vchu22@users.noreply.github.com>
2019-07-22 13:17:26 -0400

    allowed changing throwing force by dragging LMB

--- Assets/Scripts/NonVRPlayer/CameraMove.cs
+++ Assets/Scripts/NonVRPlayer/CameraMove.cs
@@ -7,19 +7,22 @@ public class CameraMove : MonoBehaviour
     [SerializeField] private float mouseSensitivity = 2.0f;
     [SerializeField] private float smoothing = 2.0f;
     [HideInInspector] public bool enabled = true;
-    
+
     private GameObject player;
     private Vector2 smoothedVelocity;
     private Vector2 currentLookingPos;
-    private void Start(){
+    private void Start()
+    {
         Cursor.lockState = CursorLockMode.Locked;
         player = transform.parent.gameObject;
     }
-    private void Update(){
+    private void Update()
+    {
         if (this.enabled)
             RotateCamera();
     }
-    private void RotateCamera(){
+    private void RotateCamera()
+    {
         Vector2 inputValues = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
         inputValues = Vector2.Scale(inputValues, new Vector2(mouseSensitivity * smoothing, mouseSensitivity * smoothing));
         smoothedVelocity.x = Mathf.Lerp(smoothedVelocity.x, inputValues.x, 1f / smoothing);
