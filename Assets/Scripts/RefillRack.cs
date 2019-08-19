bd729e39237e558ee95d1dcbf96c0a7cbd5ead19
Vanessa Chu <vchu22@users.noreply.github.com>
2019-08-18 20:02:08 -0400

    RefillRack will generate along the rack's rotation

--- Assets/Scripts/RefillRack.cs
+++ Assets/Scripts/RefillRack.cs
@@ -4,30 +4,26 @@ using UnityEngine;
 
 public class RefillRack : MonoBehaviour {
     [SerializeField] private GameObject ballPrefab;
-    private GameObject rack;
     private Vector3 startPoint;
-    private Vector3 offset;
+    private float rotation;
     void Start () {
-        rack = GameObject.Find("Basketball rack");
-        startPoint = new Vector3(-0.4f, 0.1f, 0.16f);
-        offset = new Vector3(0.4f, -0.4f, -0.32f);
+        startPoint = new Vector3(0.16f, 0.1f, -0.4f);
+        rotation = transform.rotation.eulerAngles.y;
         Generate();
     }
-    
-    void Update () {
-        
-    }
-
     private void Generate()
     {
         Vector3 newPosition;
         GameObject newDodgeball;
-        for (int i = 0; i < 4; i++)
+        for (int h = 0; h < 4; h++) // the rack has a total of 4 layers
         {
-            for (int j = 0; j < 6; j++)
+            for (int l = 0; l < 6; l++) // generate 6 balls for each row
             {
-                float k = ((j < 3)? 0.0f: 0.1f);
-                newPosition = transform.position + startPoint + new Vector3(offset.x * j + k, offset.y * i, offset.z * ((i%2==0)? 0:1));
+                float bar = ((l < 3)? 0.0f: 0.1f); // there is a bar between the first three ball of a row and the last three. need to skip the bar to distribute the balls evenly
+                int side = ((h%2==0)? 0:1); // alternate the row's side as height increments
+                Vector3 offset = startPoint + new Vector3(-0.32f * side, -0.4f * h, 0.4f  * l + bar); 
+                offset = Quaternion.Euler(0, rotation, 0) * offset; // offset should rotate along on rack's gameObject
+                newPosition = transform.position + offset;
                 newDodgeball = Instantiate(ballPrefab, newPosition, Quaternion.identity);
             }
         }
