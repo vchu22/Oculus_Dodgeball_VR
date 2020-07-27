2fd5dd6ce1f5c6ba0eb26171b8e624c98cd12077
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-07-26 21:45:12 -0400

    upgraded to 2019.4.5f1 and fixed compatibility bug

---
+++ Assets/Scripts/ObjectScale.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (DetectVRHeadset.VRMode)
+        {
+            // Vector3 scale = transform.scale;
+            Debug.Log("Object scale: ");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
