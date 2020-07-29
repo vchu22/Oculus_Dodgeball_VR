1c6ae1cefa80d021847edd0ca99b2f3adf089911
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-07-28 23:15:38 -0400

    Scale the dodgeballs and ball rack when in VR mode

--- Assets/Scripts/RefillRack.cs
+++ Assets/Scripts/RefillRack.cs
@@ -6,8 +6,13 @@ public class RefillRack : MonoBehaviour {
     [SerializeField] private GameObject ballPrefab;
     private Vector3 startPoint;
     private float rotation;
+    private float mult = 0.8f;
     void Start () {
         startPoint = new Vector3(0.16f, 1.45f, -1.1f);
+        if (DetectVRHeadset.VRMode)
+        {
+            startPoint = startPoint * mult;
+        }
         rotation = transform.rotation.eulerAngles.y;
         Generate();
     }
@@ -21,7 +26,11 @@ public class RefillRack : MonoBehaviour {
             {
                 float bar = ((l < 3)? 0.0f: 0.1f); // there is a bar between the first three ball of a row and the last three. need to skip the bar to distribute the balls evenly
                 int side = ((h%2==0)? 0:1); // alternate the row's side as height increments
-                Vector3 offset = startPoint + new Vector3(-0.32f * side, -0.4f * h, 0.4f  * l + bar); 
+                Vector3 offset = startPoint + new Vector3(-0.32f * side, -0.4f * h, 0.4f  * l + bar);
+                if (DetectVRHeadset.VRMode)
+                {
+                    offset = offset * mult;
+                }
                 offset = Quaternion.Euler(0, rotation, 0) * offset; // offset should rotate along on rack's gameObject
                 newPosition = transform.position + offset;
                 newDodgeball = Instantiate(ballPrefab, newPosition, Quaternion.identity);
