e56bf5ab585e6e549687703ff439ff9d950dd7d2
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-08-02 21:17:40 -0400

    Added StartMenu for non-VR mode

---
+++ Assets/Scripts/StartMenu/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() { 
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("FirstLevel");
+    }
+    // If menus are in different scenes
+    public void GoToSettingsMenu()
+    {
+        SceneManager.LoadScene("SettingsMenu");
+    }
+    public void GoToStartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
