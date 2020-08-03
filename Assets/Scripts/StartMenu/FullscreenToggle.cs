d72f19db3c0a35f3ddb1cd7039beb229b4c9072e
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-08-02 23:05:53 -0400

    Added fullscreen/windowed toggle in Settings

---
+++ Assets/Scripts/StartMenu/FullscreenToggle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullscreenToggle : MonoBehaviour
+{
+    public void Fullscreen(bool is_fullscreen)
+    {
+        Screen.fullScreen = is_fullscreen;
+        Debug.Log("Fullscreen is " + is_fullscreen);
+    }
+}
